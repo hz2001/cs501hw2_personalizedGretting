1004a3b1f4f23c4d5806e99ef4c4bf08a0f96086
hz2001 <hzjh@bu.edu>
2024-09-15 10:23:44 -0400

    initial commit for hw2 q2

---
+++ app/src/main/java/com/example/cs501hw2_personalgreeting/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cs501hw2_personalgreeting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cs501hw2_personalgreeting.ui.theme.Cs501hw2_personalGreetingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Cs501hw2_personalGreetingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Cs501hw2_personalGreetingTheme {
+        Greeting("Android")
+    }
+}
